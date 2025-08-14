ac59ce6c126f9b13bf27a4e151c36f436a3a17ee
lbfagioli <lbfagioli@miuandes.cl>
2025-08-14 13:34:44 -0400

    feat: cleaner export in books controller

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,19 +1,27 @@
-exports.getBooks = (req, res) => {
+const getBooks = (req, res) => {
     res.send('getting books');
 }
 
-exports.getBook = (req, res) => {
+const getBook = (req, res) => {
     res.send(`getting book ${req.params.id}`);
 }
 
-exports.createBook = (req, res) => {
+const createBook = (req, res) => {
     res.send('creating book');
 }
 
-exports.updateBook = (req, res) => {
+const updateBook = (req, res) => {
     res.send(`updating book ${req.params.id}`);
 }
 
-exports.deleteBook = (req, res) => {
+const deleteBook = (req, res) => {
     res.send(`deleting book ${req.params.id}`);
+}
+
+module.exports = {
+    getBooks,
+    getBook,
+    createBook,
+    updateBook,
+    deleteBook
 }
