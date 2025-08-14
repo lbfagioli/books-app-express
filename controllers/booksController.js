aaa15dcfa75e1b385551e37426a07fc5c41682eb
lbfagioli <lbfagioli@miuandes.cl>
2025-08-14 13:53:00 -0400

    add semicolons to bookControllers

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,22 +1,22 @@
 const getBooks = (req, res) => {
     res.send('getting books');
-}
+};
 
 const getBook = (req, res) => {
     res.send(`getting book ${req.params.id}`);
-}
+};
 
 const createBook = (req, res) => {
     res.send('creating book');
-}
+};
 
 const updateBook = (req, res) => {
     res.send(`updating book ${req.params.id}`);
-}
+};
 
 const deleteBook = (req, res) => {
     res.send(`deleting book ${req.params.id}`);
-}
+};
 
 module.exports = {
     getBooks,
@@ -24,4 +24,4 @@ module.exports = {
     createBook,
     updateBook,
     deleteBook
-}
+};
