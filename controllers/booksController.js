29c78bcbde68f91a680899d3f4625d89f1b2f368
lbfagioli <lbfagioli@miuandes.cl>
2025-08-15 11:50:43 -0400

    feat: add mongodb, initial Book model and almost finished books controller

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,21 +1,64 @@
-const getBooks = (req, res) => {
-    res.send('getting books');
+const Book = require('../models/Book');
+
+const getBooks = async (req, res) => {
+    try {
+        const books = await Book.find({});
+        res.status(200).json(books);
+    }
+    catch (err) {
+        res.status(404).json({ error: err });
+    }
 };
 
-const getBook = (req, res) => {
-    res.send(`getting book ${req.params.id}`);
+const getBook = async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id);
+        
+        if (!book) return res.status(404).json({ error: "Not found" });
+       
+        res.status(200).json(book);
+    }
+    catch (err) {
+        res.status(404).json({ error: err });
+    }
 };
 
-const createBook = (req, res) => {
-    res.send('creating book');
+const createBook = async (req, res) => {
+    // console.log(req.body.name);
+    try {
+        const newBook = new Book(req.body);        
+        const savedBook = await newBook.save();
+        res.status(201).json(savedBook);
+    }
+    catch (err) {
+        res.status(500).json({ error: err });
+    }
 };
 
-const updateBook = (req, res) => {
-    res.send(`updating book ${req.params.id}`);
+const updateBook = async (req, res) => {
+    try {
+        const book = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        
+        if (!book) return res.status(404).json({ error: "Not found" });
+
+        res.status(200).json(book);
+    }
+    catch (err) {
+        res.status(500).json({ error: err });
+    }
 };
 
-const deleteBook = (req, res) => {
-    res.send(`deleting book ${req.params.id}`);
+const deleteBook = async (req, res) => {
+    try {
+        const book = await Book.findByIdAndDelete(req.params.id);
+        
+        if (!book) return res.status(404).json({ error: "Not found" });
+
+        res.status(204).end();
+    }
+    catch (err) {
+        res.status(500).json({ error: err });
+    }
 };
 
 module.exports = {
--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const routes = require('./routes');
+const mongoose = require('mongoose');
 
+const mongoUri = 'mongodb://localhost:27017/books-app-db';
+
+mongoose.connect(mongoUri)
+    .then(() => console.log('Mongo connected'))
+    .catch((err) => console.error('Failed to connect mongo, ', err))
+
+app.use(express.json());
 app.use('/', routes);
 
 app.listen(PORT, () => {
